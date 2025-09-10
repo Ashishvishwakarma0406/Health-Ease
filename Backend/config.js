6f6b9fc882089ef5abf978b3187bae9d3c321b48
Ashish Vishwakarma <ashishvishwakarma0606@gmail.com>
2025-09-11 03:02:37 +0530

    Deploy commit

--- Backend/config.js
+++ Backend/config.js
@@ -1,2 +1,2 @@
-export const database='mongodb+srv://HealthEase:VITians@health-ease.vkrd6.mongodb.net/User?retryWrites=true&w=majority&appName=Health-Ease';
+export const database='mongodb+srv://ashishvishwakarma0606:Split4504@split.gb0ol.mongodb.net/?retryWrites=true&w=majority&appName=Split';
 export const jwtSecret = '48u1545uA';
